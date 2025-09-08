e4b4169ed2a11e491bbb71168c32f1b5620e29ce
alool <ali.odeh.pss@gmail.com>
2025-09-08 14:26:23 +0300

    1'st

---
+++ src/main/java/com/mycompany/projnnn/Projnnn.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projnnn;
+
+/**
+ *
+ * @author alool
+ */
+public class Projnnn {
+
+    public static void main(String[] args) {
+        
+        NewJFrame f = new NewJFrame();
+        f.setVisible(true);
+        
+     }
+}
